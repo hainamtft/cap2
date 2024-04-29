00eb2e3d8fa1ab38ff471fbee13b11d96260ea67
hainam <155862587+hainamtft@users.noreply.github.com>
2024-04-29 17:15:46 +0700

    ilovey

--- 1111/home/home.js
+++ 1111/home/home.js
@@ -1,20 +1,20 @@
-function startChat() {
-    var username = document.getElementById("username").value;
-    if (username.trim() !== "") {
-        document.getElementById("login-section").style.display = "none";
-        document.getElementById("chat-section").style.display = "block";
-        document.getElementById("chat-title").innerText = "Chào " + username + ", bắt đầu trò chuyện!";
-    } else {
-        alert("Vui lòng nhập tên của bạn.");
-    }
-}
+// function startChat() {
+//     var username = document.getElementById("username").value;
+//     if (username.trim() !== "") {
+//         document.getElementById("login-section").style.display = "none";
+//         document.getElementById("chat-section").style.display = "block";
+//         document.getElementById("chat-title").innerText = "Chào " + username + ", bắt đầu trò chuyện!";
+//     } else {
+//         alert("Vui lòng nhập tên của bạn.");
+//     }
+// }
 
-function sendMessage() {
-    var userInput = document.getElementById("user-input").value;
-    if (userInput.trim() !== "") {
-        var chatBox = document.getElementById("chat-box");
-        chatBox.innerHTML += "<p><strong>Bạn:</strong> " + userInput + "</p>";
-        chatBox.scrollTop = chatBox.scrollHeight;
-        document.getElementById("user-input").value = "";
-    }
-}
+// function sendMessage() {
+//     var userInput = document.getElementById("user-input").value;
+//     if (userInput.trim() !== "") {
+//         var chatBox = document.getElementById("chat-box");
+//         chatBox.innerHTML += "<p><strong>Bạn:</strong> " + userInput + "</p>";
+//         chatBox.scrollTop = chatBox.scrollHeight;
+//         document.getElementById("user-input").value = "";
+//     }
+// }
